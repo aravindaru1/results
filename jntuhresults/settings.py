13b535353575e1a203d41802c0c85219a385f9ef
aravind <122278707+aravindaru1@users.noreply.github.com>
2024-05-24 20:27:35 +0530

    Update settings.py

--- jntuhresults/settings.py
+++ jntuhresults/settings.py
@@ -106,10 +106,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+# STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
